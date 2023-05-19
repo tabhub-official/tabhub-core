63a78bbadcc2c4003b43c1f3c03b8952ecdc39a0
Chung Quan Tin <56880684+chungquantin@users.noreply.github.com>
2023-05-19 16:47:31 +0700

    Fix service type

--- src/modules/directory/directory.service.ts
+++ src/modules/directory/directory.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CollectionRegistry } from 'src/config/firebase-config';
-import { Category } from 'src/models';
+import { Directory } from 'src/models';
 
 import { BaseCRUDService } from '../_base/baseCRUD.service';
 
 @Injectable()
-export class DirectoryService extends BaseCRUDService<Category> {
+export class DirectoryService extends BaseCRUDService<Directory> {
   constructor() {
     super(CollectionRegistry.Directory);
   }
