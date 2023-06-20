8c4ce9b553f2114411fce7ea3470da406260f028
tin-snowflake <56880684+chungquantin@users.noreply.github.com>
2023-06-20 20:12:23 +0700

    Update CORS policy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ import { MAX_FILE_SIZE } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://app.tabhub.io'],
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.use('/graphql', graphqlUploadExpress({ maxFileSize: MAX_FILE_SIZE }));
